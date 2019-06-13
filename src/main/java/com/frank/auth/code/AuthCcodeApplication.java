89e60590d6f1b0ee68f005b88245d2ec0b8c91d5
cyoung <cy880708@163.com>
2019-06-13 14:53:39 +0800

    Initial commit

---
+++ src/main/java/com/frank/auth/code/AuthCcodeApplication.java
@@ -0,0 +1,21 @@
+package com.frank.auth.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0
+ * @description：
+ * @author: Yang.Chang
+ * @project: spring-boot-auth-code
+ * @package: com.frank.auth.code、
+ * @email: cy880708@163.com
+ * @date: 2019/6/13 下午2:43
+ * @mofified By:
+ */
+@SpringBootApplication
+public class AuthCcodeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCcodeApplication.class, args);
+    }
+}
